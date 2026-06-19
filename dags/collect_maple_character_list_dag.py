0a02188b0a29f59fd7535bbc14fcd611815da042
wwchang <wanw95@gmail.com>
2026-06-19 13:17:16 +0900

    계정 관련 정보 airflow 배치 단독 처리로 수정

--- dags/collect_maple_character_list_dag.py
+++ dags/collect_maple_character_list_dag.py
@@ -1,64 +1,17 @@
 from airflow import DAG
+from common.operators import MapleApiOperator
 import pendulum
-from airflow.decorators import task
-from confluent_kafka import Producer
-import uuid
-import json
-import logging
-
-'''
-캐릭터 리스트는 별도의 입력 파라미터를 요구하지 않으므로 매일 단독 배치로 호출하여 갱신한다.
-캐릭터 상세 조회 시 매번 캐릭터명으로 API를 호출해 ocid를 조회하는 대신,
-DB에 저장된 character_list 테이블에서 캐릭터명에 해당하는 ocid를 조회하도록 구성한다.
-이를 통해 불필요한 API 호출을 줄이고, 호출 제한 부담과 응답 지연을 낮출 수 있다.
-'''
 
 with DAG(
-        dag_id='collect_maple_character_list_dag',
-        schedule='0 0 * * *',  # 매일 정각마다.
-        start_date=pendulum.datetime(2025, 12, 1, tz="Asia/Seoul"),
+        dag_id='DAG_Maple_Character_List_API',
+        schedule='0 0 * * *',  # 매일 정각 수행
+        start_date=pendulum.datetime(2025, 8, 1, tz="Asia/Seoul"),
         tags=['Maple', 'Character List'],
         description="메이플스토리 계정별 캐릭터 목록 추출",
         catchup=False
 ) as dag:
 
-    BROKER_LIST = 'broker01:9092,broker02:9092,broker03:9092'
-
-    @task
-    def publish_message(**context):
-
-        job_id = str(uuid.uuid4())
-        run_id = context['run_id']
-
-        message = {
-            "job_id" : job_id,
-            "run_id" : run_id,
-            "api_name" : 'collect_maple_character_list_dag',
-            "data_nm" : 'character/list' #호출 데이터 셋
-        }
-
-        producer = Producer({'bootstrap.servers' : BROKER_LIST})
-
-        def delivery_report(err, msg):
-            if err is not None:
-                raise Exception(f"Message delivery failed: {err}")
-            else:
-                print(
-                    f"Message delivered to {msg.topic()} "
-                    f"[partition={msg.partition()}] "
-                    f"offset={msg.offset()}"
-                )
-
-
-        producer.produce(
-            topic='collect_maple_character_list_dag',
-            value=json.dumps(message).encode('utf-8'),
-            callback=delivery_report
-        )
-
-        producer.flush(10)
-
-
-    publish_message()
-
-
+    Maple_Character_List_ETL_Task = MapleApiOperator(
+        task_id='Maple_Character_List_ETL_Task',
+        data_nm='character/list'
+    )
