119dc3c2437275ad417b1cf0722320c6e240e7c2
wwchang <wanw95@gmail.com>
2026-06-19 14:02:15 +0900

    airflow 메이플 API 호출을 위한 커스텀 operator 추가

--- common/operators/MapleApiOperator.py
+++ common/operators/MapleApiOperator.py
@@ -1,18 +1,16 @@
 from airflow.sdk.bases.operator import BaseOperator
 from airflow.sdk import Variable
-from airflow.exceptions import AirflowSkipException
-from datetime import datetime
-from airflow.providers.odbc.hooks.odbc import OdbcHook
-import requests
 import json
+from datetime import datetime
+from airflow.exceptions import AirflowSkipException
+
 
 class MapleApiOperator(BaseOperator):
+    template_fields = ('data_nm', 'ocid', 'date', 'character_skill_grade')
 
-    template_fields= ('data_nm','ocid','date','character_skill_grade')
-
-    def __init__(self,data_nm,date:str | None=None,ocid: str | None = None,character_skill_grade: str | None = None,**kwargs):
+    def __init__(self, data_nm, date: str | None = None, ocid: str | None = None,
+                 character_skill_grade: str | None = None, **kwargs):
         '''
-        메이플스토리 OpenAPI에서 캐릭터 정보를 불러오기 위한 파라미터 입력
         data_nm : 호출하고자 하는 데이터의 API 종류  "/"로 구분
         예시: 캐릭터 목록 조회 api 호출시,
         data_nm = "character/list"
@@ -20,20 +18,23 @@ class MapleApiOperator(BaseOperator):
         super().__init__(**kwargs)
         self.base_url = 'https://open.api.nexon.com/maplestory/v1/'
         self.data_nm = data_nm
-        self.headers =  {"x-nxopen-api-key" : Variable.get("x-nxopen-api-key")} #api key는 airflow의 variable로 관리
+        self.headers = {"x-nxopen-api-key": Variable.get("x-nxopen-api-key")}
         self.ocid = ocid
         self.date = date
         self.character_skill_grade = character_skill_grade
 
     def execute(self, context):
+        from airflow.providers.odbc.hooks.odbc import OdbcHook
 
         con = self._call_api(self.base_url, self.data_nm, self.headers, self.date, self.ocid,
                              self.character_skill_grade)
 
         # 메이플 API에서 제공하고 있는 skill 파라미터에서, 일부 직업은 해당사항이 없어 빈값이 들어오는 경우, 가져오지 않기 위한 로직 추가
         if 'character_skill' in con.keys():
-            if not con['character_skill']:
+            if con['character_skill'] == []:
                 raise AirflowSkipException("직업에 해당하는 스킬 정보가 존재하지 않습니다.")
+
+        data = self.json_dumping(con)
 
         # Mssql Server connect
         hook = OdbcHook(odbc_conn_id='conn-db-mssql-maple',
@@ -48,6 +49,7 @@ class MapleApiOperator(BaseOperator):
 
     def _call_api(self, base_url, data_nm, headers, date: str | None = None, ocid: str | None = None,
                   character_skill_grade: str | None = None):
+        import requests
 
         request_url = base_url + data_nm
 
@@ -87,3 +89,19 @@ class MapleApiOperator(BaseOperator):
                 contents['date'] = datetime.now().strftime("%Y-%m-%dT00:00+09:00")
 
         return contents
+
+    ## json 문자열 dumping
+    def json_dumping(self, contents: dict):
+
+        empty_list = []
+
+        if len(contents.keys()) == 1:
+            for v in contents.values():
+                empty_list.extend(v)
+
+        else:
+            empty_list = contents
+
+        json_str = json.dumps(empty_list, ensure_ascii=False)
+        json_str = json_str.replace("'", "''")  # 일부 값이 '가 들어있어 dumping 과정에서 문자열이 손상되는 경우가 있어 이를 대비하기 위해 추가
+        return json_str
