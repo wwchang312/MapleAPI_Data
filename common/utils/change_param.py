601b417123aba2feac7f08e97fb5ffee0e2011ba
wwchang <wanw95@gmail.com>
2026-06-23 15:58:58 +0900

    change_param 클래스 추가. array 형식의 param value값 맵핑 및 파라미터 리스트 작성

---
+++ common/utils/change_param.py
@@ -0,0 +1,46 @@
+
+
+class ChangeParma:
+
+    def __init__(self,param:list[str],param_name:str):
+        self.param = param
+        self.param_name = param_name
+
+    def mapping_array_alias(self):
+
+        if self.param_name == 'character_info_dataset':
+            # 캐릭터 정보 데이터 셋의 alias
+            character_info_dataset = {
+                "기본정보": "character/basic",
+                "인기도": "character/popularity",
+                "종합_능력치": "character/stat",
+                "하이퍼스탯": "character/hyper-stat",
+                "성향": "character/propensity",
+                "어빌리티": "character/ability",
+                "장착_장비": "item-equipment",
+                "장착_캐시_장비": "character/cashitem-equipment",
+                "장착_심볼": "character/symbol-equipment",
+                "적용_세트효과": "character/set-effect",
+                "장착_뷰티아이템": "character/beauty-equipment",
+                "장착_안드로이드": "character/android-equipment",
+                "장착_펫": "character/pet-equipment",
+                "스킬": "character/skill",
+                "링크스킬": "character/link-skill",
+                "V매트릭스": "character/vmatrix",
+                "HEXA코어": "character/hexamatrix",
+                "HEXA매트릭스_HEXA스탯": "character/hexamatrix-stat",
+                "무릉도장": "character/dojang",
+                "기타_능력치_영향_요소": "character/other-stat",
+                "링_익스체인지_스킬_등록_장비": "character/ring-exchange-skill-equipment",
+                "예비_특수반지_장착_정보": "character/ring-reserve-skill-equipment",
+            }
+            if not self.param:
+                param_lst=[i for i in character_info_dataset.values()]
+
+                return param_lst
+
+            else:
+                param_lst=[i for i in self.param]
+
+                return param_lst
+        return None
