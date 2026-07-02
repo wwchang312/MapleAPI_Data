a85ca4cd62fb3ea610d54466c1b7e1f5f8015eaf
wwchang <wanw95@gmail.com>
2026-07-02 11:45:11 +0900

    read 데이터로 데이터프레임 작성

--- pysparkapp/maple/character/character_list.py
+++ pysparkapp/maple/character/character_list.py
@@ -1,19 +1,11 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col,explode,from_json
+from pyspark.sql.functions import explode
 from pyspark.sql.types import  IntegerType,StringType,StructType,StructField,ArrayType
 import os
 import logging
 
 s3_access_key = os.environ['S3_ACCESS_KEY']
 s3_secret_key = os.environ['S3_SECRET_KEY']
-
-schema= StructType(
-    [
-        StructField("account_id", IntegerType()),
-        StructField("character_list", ArrayType(StringType(),True),True),
-    ]
-)
-
 
 spark = (
     SparkSession.builder
@@ -29,21 +21,14 @@ spark = (
 
 raw_df = spark.read.json("s3a://maple-character-api/character/list/20260629/data.json")
 
-print(raw_df)
+raw_df.persist()
 
+result = raw_df.select(
+        "account_id",
+        explode("character_list")
+).select("account_id", "character_list.*")
 
-# parsed_df = raw_df.select(
-#     col("id"),
-#     col("name"),
-#     col("created_at")
-# )
-#
-# parsed_df.write \
-#     .format("jdbc") \
-#     .option("url", "jdbc:mysql://my-db:3306/mydb") \
-#     .option("driver", "com.mysql.cj.jdbc.Driver") \
-#     .option("dbtable", "parsed_api_data") \
-#     .option("user", "user") \
-#     .option("password", "password") \
-#     .mode("append") \
-#     .save()
+raw_df.unpersist()
+
+print(result)
+
