42ec5c811f925513f23f2f87d4d12e82af7fc6ef
wwchang <wanw95@gmail.com>
2026-08-11 10:36:13 +0900

    hadoop conf 삭제

--- pysparkapp/maple/character/stg_data.py
+++ pysparkapp/maple/character/stg_data.py
@@ -23,12 +23,6 @@ def create_spark_session() -> SparkSession:
     return(
         SparkSession.builder
         .appName("json_loader")
-        .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000")
-        .config("spark.hadoop.fs.s3a.access.key", s3_access_key)
-        .config("spark.hadoop.fs.s3a.secret.key", s3_secret_key)
-        .config('spark.hadoop.fs.s3a.path.style.access', "true")
-        .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false")
-        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
         .getOrCreate()
     )
 
