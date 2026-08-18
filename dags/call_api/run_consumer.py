d69a592b42032cf6400d5c88fedd077185ad5a55
wwchang <wanw95@gmail.com>
2026-08-18 16:35:41 +0900

    consumer 통제 DAG 추가

---
+++ dags/call_api/run_consumer.py
@@ -0,0 +1,17 @@
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.sdk import DAG
+import pendulum
+
+with DAG(
+        dag_id='collect_maple_user_achievement_dag',
+        schedule=None,  # 매주 정각 수행
+        start_date=pendulum.datetime(2025, 8, 1, tz="Asia/Seoul"),
+        tags=['Maple','Consumer'],
+        description="메이플스토리 캐릭터 정보 consumer 실행 DAG",
+        catchup=False
+) as dag:
+
+    run_consumer = BashOperator(
+        task_id = 'run_consumer',
+        bash_command='consumers/airflow_kafka_consumer.py',
+    )
