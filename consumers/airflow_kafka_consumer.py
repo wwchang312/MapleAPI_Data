0d1ef70c34de67f89b6d634555f372f52af11877
wwchang <wanw95@gmail.com>
2026-08-18 16:07:27 +0900

    API호출 병렬작업 수정

--- consumers/airflow_kafka_consumer.py
+++ consumers/airflow_kafka_consumer.py
@@ -1,6 +1,10 @@
 from base_consumer.BaseConsumer import BaseConsumer
 from confluent_kafka import Consumer,KafkaException
+from concurrent.futures import ThreadPoolExecutor,as_completed
+from collections import defaultdict
+from utils.maple_api_requests import MapleApiRequest
 import json
+import time
 
 
 #airflow "publish_maple_api_request_dag"에서 publishing한 파라미터를 consume
@@ -25,7 +29,19 @@ class AirflowKafkaConsumer(BaseConsumer):
         try:
             while True:
                 msg_lst = self.consumer.consume(num_messages=5,timeout=1.0)
-                if msg_lst is None or len(msg_lst) == 0: continue
+                if not msg_lst:
+                    continue
+
+                valid_msgs = []
+
+                for msg in msg_lst:
+                    if msg.error():
+                        self.handle_error(msg,msg.error())
+                        continue
+                    valid_msgs.append(msg)
+
+                if not valid_msgs:
+                    continue
 
                 self.logger.info(f'message count:{len(msg_lst)}')
                 for msg in msg_lst:
@@ -35,8 +51,45 @@ class AirflowKafkaConsumer(BaseConsumer):
 
                 #kafka 메시징 큐로부터, 파라미터 추출
                 self.logger.info(f'파라미터 추출 시작')
-                msg_param_lst = [json.loads(msg.value().decode('utf-8')) for msg in msg_lst]
+
+                msg_param_lst = [
+                    json.loads(msg.value().decode('utf-8'))
+                    for msg in msg_lst
+                ]
+
                 self.logger.info(msg_param_lst)
+
+                start_time = time.time()
+
+                group_results = defaultdict(list)
+
+                with ThreadPoolExecutor(max_workers=5) as executor:
+                    futures = [
+                        executor.submit(MapleApiRequest,param)
+                        for param in msg_param_lst
+                    ]
+
+                    for future in as_completed(futures):
+                        con,table_nm = future.result()
+
+                        if isinstance(con,list):
+                            group_results[table_nm].extend(con)
+                        else:
+                            group_results[table_nm].append(con)
+
+
+                print(group_results)
+
+                self.consumer.commit(asynchronous=False)
+
+                elapsed_time = time.time() - start_time
+
+                if elapsed_time < 1:
+                    time.sleep(1-elapsed_time)
+
+
+
+
 
         except KafkaException:
             self.logger.exception("Kafka exception occurred during message consumption")
